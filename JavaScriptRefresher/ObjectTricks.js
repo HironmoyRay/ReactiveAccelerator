8bb66f1389dafcc81d8f89395cc2599c3f50a57d
hironmoy-ray <hpray35@gmail.com>
2024-10-02 12:19:55 +0600

    Object Tricks

---
+++ JavaScriptRefresher/ObjectTricks.js
@@ -0,0 +1,19 @@
+const fruits = {
+    apple: "red",
+    banana: "yellow",
+    guava: "green"
+}
+
+// const result= Object.keys(fruits);
+// const result = Object.values(fruits)
+const result = Object.entries(fruits)
+console.log(result);
+
+
+let a = "popular";
+const lang = {
+    name: "js",
+    a
+}
+console.log(Object.values(lang));
+
