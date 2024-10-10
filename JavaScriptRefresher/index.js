2a33efb6e0fd017f50a3b9f8f541e06a22abdb0c
hironmoy-ray <hpray35@gmail.com>
2024-10-10 22:41:12 +0600

    Assignment & Project

---
+++ JavaScriptRefresher/index.js
@@ -0,0 +1,16 @@
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await response.json();
+
+  console.log(data);
+}
+
+getData();
+constructor(props) {
+    super(props);
+    
+}
+console.log(''); 
+console.log('alfda');
+
+
